a7f37750d9a69415a828f51f5403179c7be4c5c4
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2021-05-18 01:12:28 +0900

    initial commit

---
+++ lib/Acme/CoC/Dice.pm
@@ -0,0 +1,39 @@
+package Acme::CoC::Dice;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::CoC::Dice - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Acme::CoC::Dice;
+
+=head1 DESCRIPTION
+
+Acme::CoC::Dice is
+
+=head1 AUTHOR
+
+bedoshi E<lt>bedoshidiary.ester.freesia@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2021- bedoshi
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
