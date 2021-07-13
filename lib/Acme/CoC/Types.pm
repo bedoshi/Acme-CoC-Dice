883d33a674a87860789b8c347382ddf7980d7e73
bedoshi <bedoshidiary.ester.freesia@gmail.com>
2021-07-14 01:11:54 +0900

    fixed a part of typeConstraints.

--- lib/Acme/CoC/Types.pm
+++ lib/Acme/CoC/Types.pm
@@ -6,10 +6,10 @@ use utf8;
 use Moose::Util::TypeConstraints;
 
 subtype 'Command',
-    as => 'Str',
-    where => sub {
+    as 'Str',
+    where {
         $_ =~ /skill|ccb|cc|[1-9][0-9]*[dD][1-9][0-9]/
     },
-    message => sub { 'invalid command' };
+    message { 'invalid command' };
 
 1;
